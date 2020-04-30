3c0267e70416a407eb5c575d324a8dc8311d1f42
Likhithanjali-2 <liki2444@gmail.com>
2020-04-29 23:48:22 -0500

    categories added

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -12,7 +12,13 @@ export class PlacesComponent implements OnInit {
   constructor() { }
   places:placeInterface[];
   searchText;
+  category:string='';
   ngOnInit(): void {
     this.places = placesData;
   }
+  setValue(cat:string):void{
+    this.searchText=cat;
+    console.log(this.searchText);
+  }
+  
 }
