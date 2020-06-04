552dcd37d5a9917394c7d9c2fd36c4f05420f767
Naveen132895 <58296938+Naveen132895@users.noreply.github.com>
2020-06-04 17:45:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
   
 const ngApp = express();  
   
-ngApp.use(express.static('./dist/travel-mama'));  
+ngApp.use(express.static('./dist/travelmama'));  
   
 ngApp.get('/*', function (request, response) {  
-    response.sendFile(path.join(__dirname, '/dist/travel-mama/index.html'));  
+    response.sendFile(path.join(__dirname, '/dist/travelmama/index.html'));  
 });  
   
 ngApp.listen(process.env.PORT || 8080); 
